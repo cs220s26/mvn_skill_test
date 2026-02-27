5d7c17cb1e2197be8d646f6bc1ffc70c12f7c7f2
Ben Coleman <colemanb@moravian.edu>
2026-02-27 11:51:51 -0500

    make classname match filename

--- src/test/java/edu/moravian/data/DataGeneratorTest.java
+++ src/test/java/edu/moravian/data/DataGeneratorTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class DataGenTest {
+public class DataGeneratorTest {
 
     @Test
     public void shouldAnswerWithTrue() {
